793a24b2bd675f25b26419274f3bb2cd84ac81d6
Robles AR <tinolabs@yandex.com>
2020-12-31 02:53:33 -0300

    add tailwindcss

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,11 @@ export default function Home() {
   return (
     <>
       <Title title="Next.js + Typescript Starter" />
-      <h1>Next.js + Typescript Starter</h1>
+      <div className="h-screen flex items-center justify-center bg-gray-200">
+        <h1 className="text-4xl font-bold text-purple-800 text-center">
+          Next.js + Typescript Starter
+        </h1>
+      </div>
     </>
   )
 }
