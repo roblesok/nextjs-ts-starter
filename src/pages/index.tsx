7db26c7b88b2804091d6e15b4b3c29ef2e3aa7e9
Robles AR <tinolabs@yandex.com>
2021-01-03 16:35:18 -0300

    change index for testing purposes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <Title title="Next.js + Typescript Starter" />
       <div className="h-screen flex items-center justify-center bg-gray-200">
         <h1 className="text-4xl font-bold text-purple-800 text-center">
-          Next.js + Typescript Starter
+          Next.js + Typescript Starter!!!
         </h1>
       </div>
     </>
